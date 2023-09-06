d1554659c732724bdfd81ce871279a553b025250
omarjafor <omarjaforchy@gmail.com>
2023-09-06 22:36:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <h1>React by JaBed</h1>
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const name = 'Omar Jafor Chowdhury'
+function Home(){
+    return <h2>Full Name: {name} </h2>
+}
+
+export default Home;
