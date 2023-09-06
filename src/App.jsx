feb858bdda6b10afc760d5ee3b604fc8a12820da
omarjafor <omarjaforchy@gmail.com>
2023-09-06 23:00:49 +0600

    props object

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,19 @@ function App() {
   return (
     <>
       <h1>React by JaBed</h1>
+      <Student/>
       <Home/>
+      <Device name='Desktop PC'/>
+      <Device name='Laptop'/>
+      <Device name='Mobile'/>
       <Person/>
       <Developer/>
     </>
   )
+}
+
+function Device(props) {
+  return <h3>This Device is: {props.name}</h3>
 }
 
 function Person() {
@@ -19,6 +27,19 @@ function Person() {
   return <div className="student">
     <h3>Hello, i am {person} age {age}</h3>
   </div>
+}
+
+function Student() {
+  return (
+    <div style={{
+      border: '2px solid yellow',
+      padding: '15px',
+      margin: '15px',
+      bordeRadius: '10px'
+    }}>
+      <h3>Hello, This is a web developer</h3>
+    </div>
+  )
 }
 
 function Developer() {
