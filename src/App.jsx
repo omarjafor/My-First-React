8a6ced4a0fd2895857e6a4e503341ac19398933c
omarjafor <omarjaforchy@gmail.com>
2023-09-06 22:42:54 +0600

    react component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import './App.css'
 import Home from './pages/Home'
 
 function App() {
-
   return (
     <>
       <h1>React by JaBed</h1>
       <Home/>
+      <Person/>
     </>
   )
+}
+
+function Person() {
+  const person = 'jabed';
+  const age = 23;
+  return <h3>Hello, i am {person} age {age}</h3>
 }
 
 export default App
