1100bfd8cc5c43fbeed0d06b2100135fe1204d37
omarjafor <omarjaforchy@gmail.com>
2023-09-06 23:26:27 +0600

    props condition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 const name = 'Omar Jafor Chowdhury'
 function Home(){
-    return <h2>Full Name: {name} </h2>
+    return <div>
+        <h2>Full Name: {name} </h2>
+        <Todo/>
+    </div>
+}
+
+function Todo(){
+    return <div>
+        
+    </div>
 }
 
 export default Home;
