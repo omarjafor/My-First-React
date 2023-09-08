5434aa2449bd04752da4797738ad4a16e1a24bdc
omarjafor <omarjaforchy@gmail.com>
2023-09-08 11:12:16 +0600

    use state

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 export default function Team(){
-
+    const [team, setTeam] = useState(11);
+    const handleAdd = () => {
+        setTeam(team+1);
+    }
+    
     const teamStyle = {
         border: '2px solid purple',
         margin: '15px',
@@ -9,7 +13,9 @@ export default function Team(){
     }
     return (
         <div style={teamStyle}>
-            <h3>Player: </h3>
+            <h3>Player: {team} </h3>
+            <button onClick={handleAdd}>Add</button> &nbsp;
+            <button>Remove</button>
         </div>
     )
 }
