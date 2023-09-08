5877e65e7fb21018f6b3d45235812a2622c7c7d6
omarjafor <omarjaforchy@gmail.com>
2023-09-08 12:00:43 +0600

    useEffect fetch

--- src/component/Friends.jsx
+++ src/component/Friends.jsx
@@ -1,9 +1,17 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import './Friends.css'
+
+
 export default function Friends(){
+    const [friends, setFriends] = useState([])
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setFriends(data))
+    }, [])
     return(
         <div className="box">
-            <h3>Friends : </h3>
+            <h3>Friends : {friends.length} </h3>
         </div>
     )
 }
