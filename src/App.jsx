fe723965bce50e895701a462c3f21b6fa8e74092
omarjafor <omarjaforchy@gmail.com>
2023-09-17 23:43:17 +0600

    problem solve

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <Home />
       <button onClick={()=> buttonClicked(5)}>Clicked</button>
 
-      {/* <Student /> */}
+      <Student name={' jabed'} age= {23} />
        <Device name='Desktop PC' price='45600'/>
       <Device name='Laptop' price='50600' />
       <Device name='Mobile' price='65600' />
@@ -49,8 +49,8 @@ function Student(props) {
       bordeRadius: '10px'
     }}>
       <h3>Hello, This is a Student</h3>
-      <p>Name:</p>
-      <p>Age: </p>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age} </p>
       <p>Grade: </p>
     </div>
   )
