0f0687bbc2143d271c06e035d2ea570390540f09
omarjafor <omarjaforchy@gmail.com>
2023-09-18 13:14:01 +0600

    problem solve

--- src/component/Friend.jsx
+++ src/component/Friend.jsx
@@ -2,9 +2,8 @@ import React from "react";
 
 export default function Friend({friend}){
     const {name, email} = friend;
-    let isRed = true;
     return(
-        <div className={`card-container ${ isRed? 'red': 'blue'}`}>
+        <div className="box">
             <h4>Name : {name} </h4>
             <h5>Email : {email} </h5>
         </div>
